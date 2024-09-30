e62949b31095958c6b72fb4cc15146b78305cc8c
Hojun Na <72805486+Rojojun@users.noreply.github.com>
2024-09-30 21:44:40 +0900

    Update main.py

--- study_bot/main.py
+++ study_bot/main.py
@@ -1,12 +1,26 @@
- import discord
- from discord.ext import commands
- ​
- intents = discord.Intents.default()
- intents.message_content = True
- bot = commands.Bot(command_prefix='!', intents=intents)
- ​
- @bot.event
- async def on_ready():
-     await bot.add_cog(HelloCommand(bot))
- ​
- bot.run('STUDY_BOT_TOKEN')
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+client = discord.Client(intents=intents)
+
+@bot.event
+async def on_ready():
+    await bot.add_cog()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+TOKEN = os.getenv('TOKEN')
+client.run(TOKEN)
